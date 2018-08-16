d947bdf6aad2716a9bb5719ca552435a08472f35
Admin <admin@Admins-MacBook-Pro.local>
2018-08-16 17:07:21 -0400

    Downloaded Dev-Dependencies for Minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
 "use strict";
 
-const gulp = require('gulp');
+const gulp = require('gulp'),
+      concat = require('gulp-concat'),
+      uglifyES = require('uglify-es'),
+      rename = require('gulp-rename'),
+      maps = require('gulp-sourcemaps'),
+      uglify = require('gulp-uglify'),
+      composer = require('gulp-uglify/composer'),
+      pump = require('pump');
+
 const nodemon = require('nodemon');
 
+gulp.task('minifyScripts',()=> {
+  
+});
+
+// default task
 gulp.task('default', () => {
 
   console.log(' - nodemon started -');
